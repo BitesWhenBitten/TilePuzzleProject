def1036e21d1f571d2c09fd8d6116db6ab4315ed
BitesWhenBitten <biteswhenprovoked776@gmail.com>
2024-05-30 16:45:06 -0400

    TilePiece.cs Design Fix
    -made primary field private
    -added public getter
    -no setter available

--- Assets/Scripts/Puzzles/TilePiece.cs
+++ Assets/Scripts/Puzzles/TilePiece.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 public class TilePiece : MonoBehaviour
 {
-    public int tileNumber = 1;
+    [SerializeField] private int tileNumber = 1;
+    public int GetTileNumber()=>tileNumber;
 
 }
